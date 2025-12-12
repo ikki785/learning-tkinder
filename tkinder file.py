46799e3d540dce7e6d8c4244ca82b5483d8f885e
ikki785 <mash78589@gmail.com>
2025-12-12 16:54:42 +0530

    Update tkinder file.py
    add fuction to button

--- tkinder file.py
+++ tkinder file.py
@@ -3,15 +3,25 @@ wind = Tk()
 wind.geometry("1080x1080")
 wind.title("tkinder")
 wind.config(background='black')
+def click():
+    print("hello")
 text1 = Label(wind,
               text="hello",
               fg = 'red',
               relief=RAISED,
               padx= 4)
+
 bu1 = Button(wind,
              text="click me",
+             bg = "#1500ff",
              width=10,
              height=5,
-             padx= 20)
+             padx=20,
+             
+             )
+
+bu1.config(activebackground='red')
 bu1.pack()
+bu1.bind("<Button-1>", lambda e: click())
+
 wind.mainloop()
