27445b5764ae122612e85ca6864d78c49346ebf9
ikki785 <mash78589@gmail.com>
2025-12-13 20:03:52 +0530

    Update tkinder file.py
    working cheackbox

--- tkinder file.py
+++ tkinder file.py
@@ -5,12 +5,15 @@ w = Tk()
 w.title("suman")
 w.geometry("300x300")
 
-def click():
-    
-    a.config(text = "you just click it")
+var = IntVar()
+def toggle():
+    if var.get() == 1:
+        a.config(text="you just clicked it")
+    else:
+        a.config(text="you just unclicked it")
 
+c = Checkbutton(w,text= "this is a checkbuton",variable=var,command=toggle)
 
-c = Checkbutton(w,text= "this is a checkbuton",command=click)
 a = Label(w,text = "click it")
 
 c.pack()
