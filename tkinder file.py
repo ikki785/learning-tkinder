5333fabca0d41e5bf59a45708031b5d4fa3021de
ikki785 <mash78589@gmail.com>
2025-12-13 19:58:21 +0530

    Update tkinder file.py
    simple checkbox in tkinder

--- tkinder file.py
+++ tkinder file.py
@@ -1,47 +1,18 @@
 from tkinter import *
-
+import random
+nv = random.choice(["jon35","suman5236","you are dumass"])
 w = Tk()
 w.title("suman")
-w.geometry("1080x1080")
-w.config(background="#faf9f7")
-def info1 ():
-    name = na.get()
-    email = na1.get()
-pa = Label(w,text = "enter your name",
-           fg = "black",
-           bg = "#cfc7c5",
-           pady=3,
-           padx=3,
-           width=20)
-na = Entry()
-na.config(relief = SUNKEN,
-          fg = "black",
-          bg = "#faf9f7",
-          width=20,
-          )
+w.geometry("300x300")
+
+def click():
+    
+    a.config(text = "you just click it")
 
 
-pa1 = Label(w,text = "enter your email",
-           fg = "black", 
-           bg= "#cfc7c5",
-           pady=3,
-           padx=3,
-           width=20)
-na1 = Entry()
-na1.config(relief=SUNKEN,
-          fg = "black",
-          bg = "#faf9f7",
-          width=20)
-bu1 = Button(w,
-             text= "next",
-             bg = "#1f21b5",
-             command=info1)
+c = Checkbutton(w,text= "this is a checkbuton",command=click)
+a = Label(w,text = "click it")
 
-pa.pack()
-na.pack()
-pa1.pack()
-na1.pack()
-bu1.pack(pady=70)
-bu1.pack()
-
+c.pack()
+a.pack()
 w.mainloop()
