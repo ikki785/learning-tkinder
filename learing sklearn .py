246b597bb8d64360a6d2b083b5b64e0b7e7f1778
ikki785 <mash78589@gmail.com>
2025-12-13 19:37:49 +0530

    Update learing sklearn .py
    predicting next year sale using sklearn

--- learing sklearn .py
+++ learing sklearn .py
@@ -1,9 +1,10 @@
-
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 modle = LinearRegression()
-x = [[1],[2],[3],[4],[5]]
-y = [20,30,40,50,60]
-modle.fit(x,y)
-a = int(input("enter your working hours"))
-b = modle.predict([[a]])[0]
-print(b)
+x  = list(map(int, input("enter year ").split()))
+x1 = [[x] for x in x]
+y = list(map(int,input("enter number of sale a codining to the year :").split()))
+modle.fit(x1,y)
+ne = int(input("enter which year data you want"))
+ae = modle.predict([[ne]])
+print(f"modle predict {ae}")
