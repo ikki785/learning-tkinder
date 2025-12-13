4310dfd514e818cf993a1dc6c788e1549011f2f5
ikki785 <mash78589@gmail.com>
2025-12-13 19:50:10 +0530

    Update learing sklearn .py
    using sklearn with matplotlib

--- learing sklearn .py
+++ learing sklearn .py
@@ -5,6 +5,16 @@ x  = list(map(int, input("enter year ").split()))
 x1 = [[x] for x in x]
 y = list(map(int,input("enter number of sale a codining to the year :").split()))
 modle.fit(x1,y)
-ne = int(input("enter which year data you want"))
-ae = modle.predict([[ne]])
-print(f"modle predict {ae}")
+last_year = x[-1]
+future_years = [last_year + i for i in range(1, 4)]
+future_x = [[i] for i in future_years]
+future_sales = modle.predict(future_x)
+
+print(f"modle predict {future_sales}")
+all_years = x + future_years
+all_sales = y + list(future_sales)
+
+plt.bar(all_years, all_sales)
+
+plt.title("prediced sales")
+plt.show()
